cfd61fc5b76743e4ba6ceb283912b8063c0b906b
Olexander Pavliuk <ipz232_poo@student.ztu.edu.ua>
2026-03-09 17:22:21 +0200

    Завершено консольний проєкт з підтримкою української мови

---
+++ SumNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Введіть перше число:");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введіть друге число:");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Сума чисел: {a + b}");
+        
+        Console.WriteLine("\nНатисніть будь-яку клавішу для виходу...");
+        Console.ReadKey();
+    }
+}
